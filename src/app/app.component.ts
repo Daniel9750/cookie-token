2ad78963100be1a50b650eb367d7092c74bf97eb
Daniel <danielsanchezgonzalez@alumno.ieselrincon.es>
2024-04-18 16:46:02 +0100

    Cookie Token

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,29 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, inject, OnInit } from '@angular/core';
+import { CookieService } from 'ngx-cookie-service';
+import { Router } from '@angular/router';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+	selector: 'app-root',
+	standalone: true,
+	templateUrl: './app.component.html',
+	imports: [ReactiveFormsModule],
 })
-export class AppComponent {
-  title = 'cookie-token';
+export class AppComponent implements OnInit {
+	cookieService = inject(CookieService);
+	router = inject(Router);
+	cookieInput = new FormControl('');
+
+	ngOnInit(): void {
+		this.cookieService.set('X-Auth-Token', '');
+    this.cookieService.set('X2-Auth-Token', '');
+	}
+
+	setCookie() {
+		this.cookieService.set('X-Auth-Token', this.cookieInput.value ?? '');
+	}
+
+  setCookie2() {
+		this.cookieService.set('X2-Auth-Token', this.cookieInput.value ?? '');
+	}
 }
